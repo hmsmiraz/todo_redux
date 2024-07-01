abfd23ff827f9b1d397f659624628112b71045c2
hmsmiraz <hmsmiraz64729@gmail.com>
2024-07-01 20:21:15 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+
+function App() {
+
+  return (
+    <>
+     <h1 className="text-center font-bold text-green-400">My Todo Application</h1>
+    <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
